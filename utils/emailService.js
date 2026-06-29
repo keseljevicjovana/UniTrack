449f3e5121300f824af8d03a13877380be9fb0c1
keseljevicjovana <jovanakeseljevic51@gmail.com>
2026-06-29 09:38:47 +0200

    Force IPv4 for nodemailer to fix Render IPv6 connectivity issue

--- utils/emailService.js
+++ utils/emailService.js
@@ -4,6 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = process.env.GMAIL_USER && process.env.GMAIL_APP_PASSWORD
   ? nodemailer.createTransport({
       service: "gmail",
+      family: 4,
+      connectionTimeout: 10000,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
